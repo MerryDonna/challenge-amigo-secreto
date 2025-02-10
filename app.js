dcfe3b5184d6a984b4182b670e7ba3b4d4d842eb
Merry Donna <gianporras12@gmail.com>
2025-02-09 19:38:35 -0500

    Se agrega la función validarNombre

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables
+let nombreAmigo=[];
+let validarnombre = document.getElementById("amigo");
 
 
-function nameagregarAmigo() {
+//funciones
+function agregarAmigo() {    
+
+    validarNombre();
+
     
+
+}
+
+function validarNombre() {
+    
+    if (validarnombre.value == '') {
+        alert("Registre un nombre");
+    }
+    else{
+
+        alert("nombre agregado");
+    }
+   
+
+        
     
 }
