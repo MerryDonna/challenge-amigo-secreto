859900477633a9182ee53b8bdd64a4df1cc20149
Merry Donna <gianporras12@gmail.com>
2025-02-13 21:45:32 -0500

    modificación de funciones

--- app.js
+++ app.js
@@ -24,24 +24,65 @@ function validarNombre() {
     function obtenerNombre(){
 
         let nombreAmigo = document.getElementById('amigo').value;
+        
         return nombreAmigo;
-    
+
      }
 
 function asignarNombre(){
+
+    let agregarnNombre = document.querySelector('#listaAmigos');    
+    listaAmigo.push(obtenerNombre());
+    //console.log(nombreAmigo);
+
+    let lista ="";
+   
+    for (let nombres = 0; nombres < listaAmigo.length; nombres++) {
+
+        lista += `<li>${listaAmigo[nombres]}</li>`;
         
-    let agregarnNombre = document.querySelector('#listaAmigos');
-    agregarnNombre.innerHTML = obtenerNombre();
-    console.log(obtenerNombre());
+        //agregarnNombre.innerHTML += `<li>${listaAmigo[nombres]}</li>`;
+
+        console.log(`Array:${listaAmigo[nombres]}`)
+        //console.log(nombres);
+
+    }
+    console.log(lista);
+    agregarnNombre.innerHTML =lista;
+        
+    
+    
+
+    
+   // agregarnNombre.innerHTML = obtenerNombre();
+    //console.log(obtenerNombre());
+
+    //agregarnNombre.innerHTML = arrayNombre();
+    //console.log(arrayNombre());
  }
- 
+
 
  function limpiarCelda(){
 
     let limpiarCelda = document.getElementById('amigo').value ="";
-    //limpiarCelda.innerHTML = "";
-    console.log(`Este es valor de la celda limpiada:${limpiarCelda}`);
     
+
  }
+
+
+ function arrayNombre(){
+
+    // let nombres = [];
+    // nombres.push(obtenerNombre);
+    // console.log(nombres);
+
+
+    // for (let array of listaAmigo) {
+    //     console.log(array)
+    // }
+
+    //return listaAmigo
+ }
+
 
 }
